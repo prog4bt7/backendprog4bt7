2641aca7827d7dd708a16ffeca384f4b65c01438
prog4bt7 cecyte <prog4bt7@gmail.com>
2025-05-28 19:32:32 +0000

    se conecta zona routes

--- lib/routes.ts
+++ lib/routes.ts
@@ -1,10 +1,13 @@
+import { ZonaRoutes } from "./modules/zona/routes/zona.routes";
 import { UsuarioRoutes } from "./modules/usuario/routes/usuario.routes";
 
 export class Routes {
 
     private usuarioRoutes: UsuarioRoutes = new UsuarioRoutes();
+    private zonaRoutes: ZonaRoutes = new ZonaRoutes();
 
     public routes(app): void {
         this.usuarioRoutes.routes(app);
+        this.zonaRoutes.routes(app);
     }
 }
